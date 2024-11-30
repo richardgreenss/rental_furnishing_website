2d50a51368ef77afd80e464ef135e061249972e9
richardgreenss <166267537+richardgreenss@users.noreply.github.com>
2024-11-30 08:16:19 +0000

    d

---
+++ Solar Panels _ Tesla_files/geo4.js
@@ -0,0 +1,17 @@
+(function(){
+  window['optimizely'] = window['optimizely'] || [];
+  window['optimizely'].push(['activateGeoDelayedExperiments', {
+    'location':{
+      'city': "ARLINGTON",
+      'continent': "NA",
+      'country': "US",
+      'region': "TX",
+      'dma': "623"
+    },
+    'ip':"64.189.4.58"
+  }]);
+})
+
+()
+
+;
